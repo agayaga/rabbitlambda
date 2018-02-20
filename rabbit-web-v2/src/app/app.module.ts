b33229702167968a70273a3364e7547387395150
Hagai Reuveni <hagair@Hagais-MacBook-Pro.local>
2018-02-20 12:40:49 +0200

    Adding the data service, http module and logger to app.module

--- rabbit-web-v2/src/app/app.module.ts
+++ rabbit-web-v2/src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
 
+import { DataService } from './services/data.service';
+import { LoggerService } from './utils/logger/logger.service';
 
 import { AppComponent } from './app.component';
 
@@ -10,9 +13,10 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule
   ],
-  providers: [],
+  providers: [DataService, LoggerService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
