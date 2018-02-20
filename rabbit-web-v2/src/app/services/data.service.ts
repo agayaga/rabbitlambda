78f12d485284015239f71abac300b78aee0336f3
Hagai Reuveni <hagair@Hagais-MBP.home>
2018-02-20 23:43:40 +0200

    login and styling

--- rabbit-web-v2/src/app/services/data.service.ts
+++ rabbit-web-v2/src/app/services/data.service.ts
@@ -6,11 +6,12 @@ import 'rxjs/add/observable/throw';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/do';
 import 'rxjs/add/operator/catch';
+import 'rxjs/add/observable/from';
 
 import { LoggerService } from '../utils/logger/logger.service';
 import { HttpClient } from '@angular/common/http';
-const GET_QUESTIONS_URL = 'https://heozkamkfc.execute-api.us-east-2.amazonaws.com/latest/db';
-const SAVE_QUESTIONS_URL = 'https://heozkamkfc.execute-api.us-east-2.amazonaws.com/latest/submit';
+
+const API_URL =   'https://heozkamkfc.execute-api.us-east-2.amazonaws.com/latest';
 
 @Injectable()
 export class DataService {
@@ -20,9 +21,16 @@ export class DataService {
     private http:HttpClient
   ){}
 
-  getQuestions(): Observable<any> {
-    this.loggerService.log(`fetching questions`);
-    return this.http.get(GET_QUESTIONS_URL)
+  getQuestions(token:string): Observable<any> {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type':  'application/json',
+        'authorization': token,
+      })
+    };
+
+    this.loggerService.log(`fetching questions: ${ JSON.stringify(httpOptions) }`);
+    return this.http.get(`${API_URL}/db`, httpOptions)
         .map(response => response)
         .do((res)=>{
             this.loggerService.log(`GOt Respons: ${ JSON.stringify(res) }`);
@@ -33,15 +41,16 @@ export class DataService {
         });
   }
 
-  saveForm(formData:any): Observable<any> {
+  saveForm(formData:any, token:string): Observable<any> {
     let bodyString:string = JSON.stringify(formData);
     const httpOptions = {
         headers: new HttpHeaders({
           'Content-Type':  'application/json',
+          'authorization': token,
         })
       };
     this.loggerService.log(`form data #: ${JSON.stringify(formData) }`);
-    return this.http.post(SAVE_QUESTIONS_URL, bodyString, httpOptions )
+    return this.http.post(`${API_URL}/submit`, bodyString, httpOptions )
         .map(response => response)
         .do((response)=>{
               this.loggerService.log(`Got response #: ${ JSON.stringify(response) }`);
@@ -51,4 +60,25 @@ export class DataService {
           return Observable.throw(`An Error occured: ${JSON.stringify(error)}`)
         });
   }
+
+  login(user:any): Observable<any> {
+    let bodyString:string = JSON.stringify(user);
+    const httpOptions = {
+        headers: new HttpHeaders({
+          'Content-Type':  'application/json',
+        })
+      };
+    this.loggerService.log(`Doing login now`);
+      return Observable.from([{ userid: 'gh56HHG78KJuu' }]);
+    // return this.http.post(`${API_URL}/logon`, bodyString, httpOptions )
+    //     .map(response => response)
+    //     .do((response)=>{
+    //           this.loggerService.log(`Got response #: ${ JSON.stringify(response) }`);
+    //       })
+    //     .catch((error:any) =>{
+    //       this.loggerService.log(`An Error occured: ${JSON.stringify(error)}`);
+    //       return Observable.throw(`An Error occured: ${JSON.stringify(error)}`)
+    //     });
+  }
+  
 }
