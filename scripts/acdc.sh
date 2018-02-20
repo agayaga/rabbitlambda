9331a28b2e571d5a1ba1c54af9b43ada8bc62463
Ram <ram-on@tikalk.com>
2018-02-20 16:51:48 +0200

    now installing also angular cli

--- scripts/acdc.sh
+++ scripts/acdc.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 # make sure you install anguler-cli (after installing npm) using:
-# sudo npm install --unsafe-perm  -g @angular/cli
 
+sudo npm install --unsafe-perm  -g @angular/cli
 GiTHuB=https://github.com/agayaga/rabbitlambda
-
 GITDIR=$(mktemp -d)
 echo GITDIR=${GITDIR}
 pushd ${GITDIR}
