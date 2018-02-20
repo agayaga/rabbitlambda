4bcbef541d8d9188247ba5d78eacd8609d3bb99f
Ram <ram-on@tikalk.com>
2018-02-20 17:04:01 +0200

    now working on master

--- scripts/acdc.sh
+++ scripts/acdc.sh
@@ -6,9 +6,7 @@ DIR=${HOME}/.aws
 echo "[default]" > ${DIR}/credentials 
 echo aws_access_key_id=${aws_access_key_id} >> ${DIR}/credentials 
 echo aws_secret_access_key=${aws_secret_access_key} >> ${DIR}/credentials 
-echo -e "[default]\nregion = us-east-1" > ${DIR}/config
-
-
+echo -e "[default]\nregion = us-east-2" > ${DIR}/config
 
 sudo npm install --unsafe-perm  -g @angular/cli
 GiTHuB=https://github.com/agayaga/rabbitlambda
@@ -16,7 +14,8 @@ GITDIR=$(mktemp -d)
 echo GITDIR=${GITDIR}
 pushd ${GITDIR}
   echo "PwD=${PWD}"
-  git clone -b rabbit-web-app ${GiTHuB}
+  git clone ${GiTHuB}
+#  git clone -b rabbit-web-app ${GiTHuB}
   echo "PwD=${PWD}"
   cd rabbitlambda/rabbit-web-v2
   npm install
