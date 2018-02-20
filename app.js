becdbb720e6788bc617d6874f6cafb943860db5c
Shahar Solomianik <shahar@gnift.com>
2018-02-20 11:34:20 +0200

    claudia

---
+++ app.js
@@ -0,0 +1,10 @@
+var ApiBuilder = require('claudia-api-builder'),
+  api = new ApiBuilder();
+
+module.exports = api;
+
+var db = require('./db.json')
+
+api.get('/db', function () {
+  return db;
+});
